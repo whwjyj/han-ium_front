73f6a1e665b1ee5dfa6d83ba182ff4e6100f9407
hihilong <kim73715327@gmail.com>
2024-08-11 08:01:42 +0900

    작업 중인 변경 사항 커밋

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+import Login from './login/login';
+import SignUp from './login/login page';
+import LoginMain from './login/login main';
 import HomeScreen from './src/screen/Home';
 
 
@@ -13,6 +17,7 @@ const App = () => {
       <Stack.Navigator
         initialRouteName="아파트"  >
         <Stack.Screen name="아파트" component={HomeScreen} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -21,3 +26,5 @@ const App = () => {
 
 
 export default App;
+
+
